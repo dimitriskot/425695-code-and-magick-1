a5bad8eaa2033f9ccfe05c9f47eb87407d74f1fb
dimitriskot <mopo_kdi-web@mail.ru>
2018-02-09 19:47:04 +0300

    Correction #1

--- js/stat.js
+++ js/stat.js
@@ -1,69 +1,72 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  var rectX = 100;
-  var rectY = 10;
-  var rectWidth = 420;
-  var rectHeight = 270;
-  var textX = 120;
-  var textY = 40;
+  var RECT_X = 100;
+  var RECT_Y = 10;
+  var RECT_WIDTH = 420;
+  var RECT_HEIGHT = 270;
+  var TEXT_X = 120;
+  var TEXT_Y = 40;
+  var CLOUD_SHADOW_X = 10;
+  var CLOUD_SHADOW_Y = 10;
+  var CLOUD_COLOR = 'white';
+  var SHADOW_TEXT_X = 0;
+  var SHADOW_TEXT_Y = 0;
+  var TEXT_TITLE = 'Ура вы победили!';
+  var TEXT_RESULTS = 'Список результатов:';
+  var TEXT_FONT = '16px PT Mono';
+  var TEXT_COLOR = 'black';
 
   ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.fillStyle = 'white';
-  ctx.strokeRect(rectX, rectY, rectWidth, rectHeight);
-  ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+  ctx.shadowOffsetX = CLOUD_SHADOW_X;
+  ctx.shadowOffsetY = CLOUD_SHADOW_Y;
+  ctx.fillStyle = CLOUD_COLOR;
+  ctx.strokeRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
+  ctx.fillRect(RECT_X, RECT_Y, RECT_WIDTH, RECT_HEIGHT);
 
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
-  ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', textX, textY);
-  ctx.fillText('Список результатов:', textX, textY + 20);
+  ctx.shadowOffsetX = SHADOW_TEXT_X;
+  ctx.shadowOffsetY = SHADOW_TEXT_Y;
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_FONT;
+  ctx.fillText(TEXT_TITLE, TEXT_X, TEXT_Y);
+  ctx.fillText(TEXT_RESULTS, TEXT_X, TEXT_Y + 20);
 
-  var sortHistogram = function () {
-    for (var i = 0; i <= times.length - 2; i++) {
-      var maxTime = times[i];
-      var maxName = names[i];
-      for (var j = i + 1; j <= times.length - 1; j++) {
-        if (times[j] > maxTime) {
-          maxTime = times[j];
-          maxName = names[j];
-          var swapTime = times[i];
-          var swapName = names[i];
-          times[i] = maxTime;
-          names[i] = maxName;
-          times[j] = swapTime;
-          names[j] = swapName;
-        }
+  var getMaxElement = function (arr) {
+    var maxElement = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
       }
+    }
+    return maxElement;
+  };
+  var histogramHeight = 150;
+  var maxTime = getMaxElement(times);
+  var step = histogramHeight / maxTime;
+
+  var getColor = function (name) {
+    var PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
+    var PLAYERS_COLOR = 'rgba(28, 48, 251, ' + Math.random() + ')'
+    if (name !== 'Вы') {
+      ctx.fillStyle = PLAYERS_COLOR;
+    } else {
+      ctx.fillStyle = PLAYER_COLOR;
     }
   };
 
-  sortHistogram();
-
-  var histogramHeight = 150;
-  var step = histogramHeight / (times[0] - 0);
-
-  var barWidth = 40;
-  var indent = 80;
-  var initialX = 140;
-  var initialY = 240;
-  var lineHeight = 20;
-
   var getHistogram = function () {
+    var barWidth = 40;
+    var indent = 80;
+    var initialX = 140;
+    var initialY = 240;
+    var lineHeight = 20;
     for (var i = 0; i < times.length; i++) {
       var indentation = indent * i;
       var time = times[i];
       var name = names[i];
-      if (name !== 'Вы') {
-        ctx.fillStyle = 'rgba(28, 48, 251, ' + Math.random(0.11, 1.01) + ')';
-      } else {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      }
+      getColor(name);
       ctx.fillRect(initialX + indentation, initialY, barWidth, -(time * step));
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = TEXT_COLOR;
       ctx.fillText(Math.floor(time), initialX + indentation, initialY - lineHeight / 2 - time * step);
       ctx.fillText(name, initialX + indentation, initialY + lineHeight);
     }
